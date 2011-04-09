7c50289605c75869329ca4c4a72bdfab6c0974cd
Eugen Dahm <eugen.dahm@gmail.com>
2011-04-09 17:55:36 +0200

    Templates for relations added, OneToOne should work

--- modelDrivenSwag/src-gen/Address.java
+++ modelDrivenSwag/src-gen/Address.java
@@ -1,33 +1,58 @@
+import javax.persistence.*;
+@Entity
 public class Address {
 
+	private Long id;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	private String street;
+	private String zip;
+	private String city;
+
+	@Column
+	public String getStreet() {
+		return street;
+	}
 
 	public void setStreet(String street) {
 		this.street = street;
 	}
 
-	public String getStreet() {
-		return street;
+	@Column
+	public String getZip() {
+		return zip;
 	}
-
-	private String zip;
 
 	public void setZip(String zip) {
 		this.zip = zip;
 	}
 
-	public String getZip() {
-		return zip;
+	@Column
+	public String getCity() {
+		return city;
 	}
-
-	private String city;
 
 	public void setCity(String city) {
 		this.city = city;
 	}
 
-	public String getCity() {
-		return city;
+	private Person person;
+	@OneToOne(mappedBy = "person")
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person varPerson) {
+		person = varPerson;
 	}
 
 }
--- modelDrivenSwag/src-gen/Person.java
+++ modelDrivenSwag/src-gen/Person.java
@@ -1,33 +1,49 @@
+import javax.persistence.*;
+@Entity
 public class Person {
 
+	private Long id;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	private String name;
+	private Integer age;
+	private Address address;
+
+	@Column
+	public String getName() {
+		return name;
+	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getName() {
-		return name;
+	@Column
+	public Integer getAge() {
+		return age;
 	}
-
-	private Integer age;
 
 	public void setAge(Integer age) {
 		this.age = age;
 	}
 
-	public Integer getAge() {
-		return age;
+	@Column
+	@OneToOne
+	public Address getAddress() {
+		return address;
 	}
-
-	private Address address;
 
 	public void setAddress(Address address) {
 		this.address = address;
-	}
-
-	public Address getAddress() {
-		return address;
 	}
 
 }
