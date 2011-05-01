b06e37f288c0022e00d7cc08c19f7d8a429f64c3
Hubert <vevusio@gmail.com>
2011-05-01 21:16:20 +0200

    tests

--- modelDrivenSwag/tests/tests/swag/db/daotests/UserTest.java
+++ modelDrivenSwag/tests/tests/swag/db/daotests/UserTest.java
@@ -23,13 +23,35 @@ public class UserTest extends AbstractDBTest {
 	
 	@Test
 	public void createUser() {
-		User user = new User();
-		user.setName("lol");
-		user.setPassword("wat");
-		user.setSalt("omfg");
-		user.setTimezone(1);
-		user.setUsername("hahahaha");
-		user.setAddress("wonderland");
+		userDAO.persist(createUser("a", "a", "a", 1, "a", "a"));
+	}
+	
+	@Test
+	public void readUser() {
+		User user = createUser("b", "b", "b", 1, "b", "b");
 		userDAO.persist(user);
+		int id = user.getId();
+		User read = userDAO.get(id);
+		Assert.assertEquals(user, read);
+	}
+	
+	@Test
+	public void deleteUser() {
+		User user = createUser("c", "c", "c", 1, "c", "c");
+		userDAO.persist(user);
+		int id = user.getId();
+		userDAO.delete(user);
+		Assert.assertNull(userDAO.get(id));
+	}
+	
+	public static User createUser(String name, String pw, String salt, int timeZone, String userName, String address) {
+		User user = new User();
+		user.setName(name);
+		user.setPassword(pw);
+		user.setSalt(salt);
+		user.setTimezone(1);
+		user.setUsername(userName);
+		user.setAddress(address);
+		return user;
 	}
 }
