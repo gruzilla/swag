c22e92ce6e0fe00eeb7dc384cf9e6ee776b55974
Philipp <philipp.raich@gmail.com>
2011-06-11 20:41:13 +0200

    another Mailer update... :S

--- modelDrivenSwag/src/swag/Mailer.java
+++ modelDrivenSwag/src/swag/Mailer.java
@@ -5,41 +5,65 @@ import javax.mail.internet.*;
 import java.util.*;
 
 public class Mailer {
+	
+	private static final String SMTP_HOST_NAME = "myserver.smtphost.com";
+	private static final String SMTP_AUTH_USER = "myusername";
+	private static final String SMTP_AUTH_PWD  = "mypwd";
+	private static final boolean SMTP_AUTH_SET = true;
+	
 	public void postMail( String recipients[ ], String subject, String message , String from) throws MessagingException
 	{
-	    boolean debug = false;
 
+	    boolean debug = false;
+	    
 		//Set the host smtp address
 		Properties props = new Properties();
-		props.put("mail.smtp.host", "email.aon.at");
-		props.put("mail.smtp.user", "p.raich@aon.at");
-		props.put("mail.smtp.password", "email");
-	
-	    // create some properties and get the default Session
-	    Session session = Session.getDefaultInstance(props, null);
-	    session.setDebug(debug);
-	
+		props.put("mail.smtp.host", SMTP_HOST_NAME);
+		props.put("mail.smtp.auth", SMTP_AUTH_SET);
+
+		Session session = null;
+	    if (SMTP_AUTH_SET) {
+	    	Authenticator auth = new SMTPAuthenticator();
+	    	session = Session.getInstance(props, auth);
+	    } else {
+	    	session = Session.getInstance(props);
+		}
+
+		session.setDebug(debug);
+
 	    // create a message
 	    Message msg = new MimeMessage(session);
-	
+
 	    // set the from and to address
 	    InternetAddress addressFrom = new InternetAddress(from);
 	    msg.setFrom(addressFrom);
-	
-	    InternetAddress[] addressTo = new InternetAddress[recipients.length]; 
+
+	    InternetAddress[] addressTo = new InternetAddress[recipients.length];
 	    for (int i = 0; i < recipients.length; i++)
 	    {
 	        addressTo[i] = new InternetAddress(recipients[i]);
 	    }
 	    msg.setRecipients(Message.RecipientType.TO, addressTo);
-	   
-	
-	    // Optional : You can also set your custom headers in the Email if you Want
-	    msg.addHeader("MyHeaderName", "myHeaderValue");
-	
+
+
 	    // Setting the Subject and Content Type
 	    msg.setSubject(subject);
 	    msg.setContent(message, "text/plain");
 	    Transport.send(msg);
+	}
+	
+	/**
+	* SimpleAuthenticator is used to do simple authentication
+	* when the SMTP server requires it.
+	*/
+	private class SMTPAuthenticator extends javax.mail.Authenticator
+	{
+
+		public PasswordAuthentication getPasswordAuthentication()
+	    {
+	        String username = SMTP_AUTH_USER;
+	        String password = SMTP_AUTH_PWD;
+	        return new PasswordAuthentication(username, password);
+	    }
 	}
 }
