dfa605355fc4ed783bac078bf1cc76bb5aa24fd1
Philipp <philipp.raich@gmail.com>
2011-06-11 15:43:47 +0200

    removed context from UserSession

--- modelDrivenSwag/src/swag/rest/session/UserSession.java
+++ modelDrivenSwag/src/swag/rest/session/UserSession.java
@@ -3,8 +3,6 @@ package swag.rest.session;
 import java.io.Serializable;
 
 import javax.ejb.Stateful;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 
 import swag.db.model.User;
 
@@ -16,13 +14,10 @@ public class UserSession implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	@Context
-	private HttpHeaders headers;
-
 	private User user;
 	
 	public UserSession() {
-		System.out.println("we have headers: "+(headers==null));
+		System.out.println("we have headers: ");
 	}
 	
 	public void setUser(User user) {
