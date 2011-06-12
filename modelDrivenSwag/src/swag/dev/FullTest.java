f31c50b52eb2bba1ba4e6545b08335f80aa18a93
Matthias Steinböck <grillen@abendstille.at>
2011-06-12 18:35:05 +0200

    fulltest chekc

--- modelDrivenSwag/src/swag/dev/FullTest.java
+++ modelDrivenSwag/src/swag/dev/FullTest.java
@@ -7,24 +7,25 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Cookie;
 
 import swag.rest.session.UserSession;
 import swag.singletons.Chats;
 
-
-@Stateful
-@SessionScoped
 @Path("full")
 public class FullTest {
 	
 	@PersistenceContext(type=PersistenceContextType.EXTENDED)
 	EntityManager em;
 	
-	@Inject
-	UserSession session;
+	@Context
+	HttpServletRequest request;
 	
 	@EJB
 	Chats chats;
@@ -37,7 +38,7 @@ public class FullTest {
 	
 	@GET
 	@Produces("application/json")
-	public String get() {
+	public String get(@CookieParam("sessionId") String rofl) {
 		
 		/*
 		if (wsContext == null) { return "ws is null"; }
@@ -47,15 +48,17 @@ public class FullTest {
 		return "amount: "+wsContext.getMessageContext().get("counter");
 		*/
 		return "normaL amount: "+beanCounter+"\n"+
-			(session==null?"sessnull":"valid?:"+session.isValid())+"\n"+
+			(request==null?"reqNULL":"YYEEEEE:"+request.getCookies())+"\n"+
 			(em==null?"emnull":"emexists")+"\n"+
+			(rofl==null?"sessionId null":"cookie: "+rofl)+"\n"+
+			"via cookie class: "+Cookie.valueOf("sessionId").getValue()+"\n"+
 			(chats==null?"chatsnull":"chatsexist: "+(chats.getManager()==null?"singleemNull" : "single em exists"))+"\n";
 	}
 	
 	@GET
 	@Path("inc")
 	@Produces("application/json")
-	public String increment() {
+	public String increment(@CookieParam("sessionId") String rofl) {
 		/*
 		if (wsContext == null) { return "ws is null"; }
 		Integer counter = 0;
@@ -69,7 +72,9 @@ public class FullTest {
 		*/
 		beanCounter++;
 		return "incamount: "+beanCounter+"\n"+
-		(session==null?"sessnull":session.isValid())+"\n"+
+		(request==null?"reqNULL":"YYEEEEE:"+request.getCookies())+"\n"+
+		(rofl==null?"sessionId null":"cookie: "+rofl)+"\n"+
+		"via cookie class: "+Cookie.valueOf("sessionId").getValue()+"\n"+
 		(em==null?"emnull":"em exists")+"\n";
 	}
 	
@@ -90,7 +95,7 @@ public class FullTest {
 		*/
 		beanCounter--;
 		return "decamount: "+beanCounter+"\n"+
-		(session==null?"sessnull":session.isValid())+"\n"+
+		(request==null?"reqNULL":"YYEEEEE:"+request.getCookies())+"\n"+
 		(em==null?"emnull":"em exists")+"\n";
 	}
 }
