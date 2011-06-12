ebe5f7ffc02d1e36728db02eb6c62b91a001a9fe
Philipp <philipp.raich@gmail.com>
2011-06-12 11:30:07 +0200

    added TLS to the Mailer

--- modelDrivenSwag/src/swag/Mailer.java
+++ modelDrivenSwag/src/swag/Mailer.java
@@ -3,13 +3,17 @@ package swag;
 import javax.mail.*;
 import javax.mail.internet.*;
 import java.util.*;
+import java.lang.Boolean;
 
 public class Mailer {
 	
-	private static final String SMTP_HOST_NAME = "myserver.smtphost.com";
-	private static final String SMTP_AUTH_USER = "myusername";
-	private static final String SMTP_AUTH_PWD  = "mypwd";
+	private static final String SMTP_HOST_NAME = "smtp.gmail.com";
+	private static final String SMTP_HOST_PORT = "465";
+	private static final String SMTP_AUTH_USER = "p.raich@aon.at";
+	private static final String SMTP_AUTH_PWD  = "whattheduck";
 	private static final boolean SMTP_AUTH_SET = true;
+	private static final boolean SMTP_TLS_SET = true;
+
 	
 	public void postMail( String recipients[ ], String subject, String message , String from) throws MessagingException
 	{
@@ -19,7 +23,14 @@ public class Mailer {
 		//Set the host smtp address
 		Properties props = new Properties();
 		props.put("mail.smtp.host", SMTP_HOST_NAME);
-		props.put("mail.smtp.auth", SMTP_AUTH_SET);
+		props.put("mail.smtp.auth", new Boolean(SMTP_AUTH_SET).toString());
+		props.put("mail.smtp.user", SMTP_AUTH_USER);
+		props.put("mail.smtp.port", SMTP_HOST_PORT);
+		props.put("mail.smtp.starttls.enable",SMTP_TLS_SET);
+		//props.put("mail.smtp.debug", "true");
+		props.put("mail.smtp.socketFactory.port", SMTP_HOST_PORT);
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.socketFactory.fallback", "false");
 
 		Session session = null;
 	    if (SMTP_AUTH_SET) {
